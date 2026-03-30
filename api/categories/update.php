31fd7749c5e1c168829fbbd528f8f4291eda8bba
Thomas Zermeno <madcarrots@users.noreply.github.com>
2026-03-30 16:16:52 -0500

    Update update.php
    remove includes

--- api/categories/update.php
+++ api/categories/update.php
@@ -5,8 +5,6 @@
     header('Access-Control-Allow-Methods: PUT');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Allowed-Methods, Authorization, X-Requested_With');
 
-    include_once '../config/Database.php';
-    include_once '../models/Author.php';
 
     // instantiate database and connect
     $database = new Database();
