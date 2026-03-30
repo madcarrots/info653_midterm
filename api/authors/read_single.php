ccd381690df5c68a1f4e93231bbd6b53d1b6f506
Thomas Zermeno <madcarrots@users.noreply.github.com>
2026-03-30 16:14:50 -0500

    Update read_single.php
    remove includes

--- api/authors/read_single.php
+++ api/authors/read_single.php
@@ -3,9 +3,6 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: appplication/json');
 
-    include_once '../config/Database.php';
-    include_once '../models/Author.php';
-
     // instantiate DB and Connect
     $database = new Database();
     $db = $database->connect();
